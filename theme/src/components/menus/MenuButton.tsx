e86e599d0eea2df3d577f6fbd3bb9e96f74cee0a
Matt Reimer <matt@northarrowresearch.com>
2024-05-02 14:49:27 -0700

    Cleaning up the menu links

--- theme/src/components/menus/MenuButton.tsx
+++ theme/src/components/menus/MenuButton.tsx
@@ -8,10 +8,11 @@ import { Link as GatsbyLink } from 'gatsby'
 
 interface LinkButtonProps extends ButtonProps {
   to: string
+  target?: string
 }
 
-const MenuButton: React.FC<LinkButtonProps> = ({ to, ...props }) => {
-  const CustomLinkStub = (props, ref) => <GatsbyLink to={to} {...props} ref={ref} />
+const MenuButton: React.FC<LinkButtonProps> = ({ to, target, ...props }) => {
+  const CustomLinkStub = (props, ref) => <GatsbyLink to={to} {...props} ref={ref} target={target} />
   const CustomLink = React.forwardRef(CustomLinkStub)
 
   return <Button variant="text" component={CustomLink} {...props} />
