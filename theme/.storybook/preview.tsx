1f8d285757a898708b68e5d3519ca2c5b5b02aef
Matt <1063391+MattReimer@users.noreply.github.com>
2024-07-12 16:02:43 +0000

    adding MUI storybook preview wrapper

--- theme/.storybook/preview.tsx
+++ theme/.storybook/preview.tsx
@@ -1,6 +1,17 @@
+
+import React from 'react'
+import { ThemeProvider } from '@mui/system'
 import type { Preview } from '@storybook/react'
+import Layout from '../src/components/LayoutStorybook'
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <Layout>
+        <Story />
+      </Layout>
+    ),
+  ],
   parameters: {
     controls: {
       matchers: {
