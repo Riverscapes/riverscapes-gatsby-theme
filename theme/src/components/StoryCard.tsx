c3a71087a87bad625a7c241634700b7b3a1becd6
Matt Reimer <matt@northarrowresearch.com>
2025-01-03 11:01:38 -0500

    fixing storycards #36

--- theme/src/components/StoryCard.tsx
+++ theme/src/components/StoryCard.tsx
@@ -1,5 +1,6 @@
 import { Box, Card, CardActionArea, CardContent, Grid, Typography } from '@mui/material'
 import React from 'react'
+import { withPrefix } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import { ContentLink as Link } from './ContentLink'
 
@@ -13,13 +14,20 @@ interface StoryCardProps {
 
 export const StoryCard: React.FC<StoryCardProps> = ({ title, to, description, image, imageAlt }) => {
   const imgIsString = typeof image === 'string'
+  // If the image is a relative path, append the pathPrefix from gatsby-config.js
+  let newSrc: string = image
+  if (imgIsString) {
+    if (newSrc.startsWith('/')) {
+      newSrc = withPrefix(newSrc)
+    }
+  }
   return (
     <Grid item xs={12} md={6} lg={3}>
       <Card sx={{ height: '100%' }}>
         <CardActionArea component={Link} to={to}>
           {imgIsString ? (
             <Box>
-              <img src={image} alt={imageAlt} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+              <img src={newSrc} alt={imageAlt} style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
             </Box>
           ) : (
             <GatsbyImage image={image} alt={imageAlt} />
